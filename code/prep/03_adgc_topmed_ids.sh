82a7c5084ea44bbc6d4d520218fda997b4b597a5
Lincoln Shade <lincoln.shade@uky.edu>
2021-06-09 13:23:11 -0400

    retrieve NACCIDs from ADGC TOPMed

---
+++ code/prep/03_adgc_topmed_ids.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#==================================================
+# list NACCIDs in ADGC imputed using TOPMed panel
+# (run in gazelle)
+#==================================================
+
+for i in {1..12}
+  do 
+  file=$(ls /data_global/ADGC_GWAS/ADGC_NHW/ADC$i/RawGenotypes/ | egrep '.*.fam')
+  cat /data_global/ADGC_GWAS/ADGC_NHW/ADC$i/RawGenotypes/$file | awk '{print$2}' >> data/tmp/nacc_ids_adgc_topmed.txt
+  done
